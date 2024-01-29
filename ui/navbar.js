8e1e89870c2b1061fc820685cdbaef7628fdc664
mrtyilmaz1 <muryilmaz95@gmail.com>
2024-01-29 22:18:24 +0300

    Update for navbar

--- ui/navbar.js
+++
@@ -1,6 +0,0 @@
-fetch('navbar.html')
-  .then(response => response.text())
-  .then(html => {
-    // Navbar içeriğini navbarContainer elementine ekle
-    document.getElementById('navbarContainer').innerHTML = html;
-  });
