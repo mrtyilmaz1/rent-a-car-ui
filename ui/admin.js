d587eb984d91e6a331b1a525641b68ceff830d66
mrtyilmaz1 <muryilmaz95@gmail.com>
2024-02-19 22:54:32 +0300

    update admin.js

--- ui/admin.js
+++ ui/admin.js
@@ -68,8 +68,90 @@ async function addProduct() {
     getAllProduct();
     resetAddProductForm(); // Modal formunu sıfırla.
     //backende istek at
-    $('#addProductModal').modal('hide'); // Modal'ı kapat.
-
-    
+    $('#addProductModal').modal('hide'); // Modal'ı kapat.   
 
 }
+
+//product tablosunu güncellemek için
+async function renderProductTable() {
+
+    const productTableBody = document.getElementById("productTableBody");
+    productTableBody.innerHTML = "";
+
+    products.forEach(product => {
+
+        const row = productTableBody.insertRow();
+        row.innerHTML = `
+        <td> ${product.brandId} </td>
+        <td> ${product.brand } </td>
+        <td> ${product.model} </td>
+        <td> ${product.color } </td>
+        <td> ${product.gear} </td>
+        <td> ${product.price} </td>
+        <td> ${product.year} </td>
+        <td> ${product.totalKm} </td>    
+        <td> ${product.unitsInStock} </td>
+        <td><img src="C:\\Users\\hasan.demircan\\Desktop\\e-commerce\\${product.image}" alt="${product.name}" style="max-width: 50px; ax-height: 50px;"> </td>
+        <td> ${product.active ? 'Yes' : 'No'} </td>
+        <td>
+            <button class="btn btn-warning btn-sm" onclick="editProduct(${product.id})">Güncelle</button>
+            <button class="btn btn-danger btn-sm" onclick="deleteProduct(${product.id})">Sil</button>
+        </td>    
+      `;
+    });
+}
+async function getAllProduct() {
+
+    try {
+        const response = await fetch('http://localhost:8081/car/all', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + jwtToken
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch products');
+        }
+
+        products = await response.json();
+        await renderProductTable(); // Yeni ürünü tabloya ekleyerek güncelle.
+
+
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+
+}
+
+// Ürünü silmek için
+function deleteProduct(productId) {
+    const confirmed = confirm("Are you sure you want to delete this product?");
+    if (confirmed) {
+        // Silme işlemi burada gerçekleştirilebilir.
+        fetch('http://localhost:8081/car/' + productId, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + jwtToken
+            },
+        })
+            .then(response => {
+                if (response.ok) {
+                    // Başarılı ise tabloyu güncelle
+
+                    products = products.filter(product => product.id !== productId);
+                    renderProductTable();
+                } else {
+                    // Başarısız ise hata mesajını logla
+                    console.error('Error:', response.status);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+    }
+}
+
